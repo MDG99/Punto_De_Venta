2c49b84b9bb9e27d919cb915cde258d729bce5a7
Ignacio Isaac <ignacioisaac30@gmail.com>
2019-05-18 02:12:25 -0500

    Pantalla de Alta de productos

---
+++ Punto_De_Venta/Punto_De_Venta/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto_De_Venta
+{
+    public partial class AltaProductos : Form
+    {
+        public AltaProductos()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
